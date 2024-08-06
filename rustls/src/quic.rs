cbc275df7b0fe33b45ffae13d2d6be2b63b6cc09
0xDEADBEAT <jasperbriggs677@gmail.com>
2024-08-05 20:46:12 -0400

    ?

--- rustls/src/quic.rs
+++ rustls/src/quic.rs
@@ -675,7 +675,7 @@ impl PacketKey {
     ) -> Result<Tag, Error> {
         let aad = aead::Aad::from(header);
         let nonce = nonce_for(packet_number, &self.iv);
-        debug!("rustsl enc aad-header is: {}, nonce is: {}", header, packet_number);
+        debug!("rustsl enc aad-header is: {:?}, nonce is: {}", header, packet_number);
         let tag = self
             .key
             .seal_in_place_separate_tag(nonce, aad, payload)
@@ -699,7 +699,7 @@ impl PacketKey {
         let payload_len = payload.len();
         let aad = aead::Aad::from(header);
         let nonce = nonce_for(packet_number, &self.iv);
-        debug!("rustsl dec aad-header is: {}, nonce data is: {}", header, packet_number);
+        debug!("rustsl dec aad-header is: {:?}, nonce data is: {}", header, packet_number);
         self.key
             .open_in_place(nonce, aad, payload)
             .map_err(|_| Error::DecryptError)?;
